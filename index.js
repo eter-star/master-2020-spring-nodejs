1d9de12442f4178f52f8b52767ff5a268954a41e
Song WANG <wangs.lisa@gmail.com>
2020-07-28 11:12:55 +0200

    second commit

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-require('dotenv').config()
+require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
-const twsRouter = require('./routes/tws');
+const twsRouter = require('./routes/web/tws');
+const twsApiRouter = require('./routes/api/tws');
+
+const connectDB = require('./middlewares/db');
+const { Router } = require('express');
+connectDB.connect();
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended:false }))
+app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/tws',twsRouter);
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
 
-app.get('/',function(req, res){
-    res.send('It works!')
+ app.use(express.static(path.join(__dirname, 'public')));
+//app.use(new Router(path.join(__dirname,'public')))
+
+app.use('/tws', twsRouter);
+app.use('/api/tws', twsApiRouter);
+
+app.get('/', function(req, res) {
+    res.send('It works !');
 });
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     const error = new Error('Not Found !');
     error.status = 404;
     next(error);
 });
 
-// app.get('/tws',function(req, res){
-//     res.send('tws');
-// });
-
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT, function() {
     console.log('Server running on localhost:' + process.env.PORT);
 });
+
+// connectDB.close();
